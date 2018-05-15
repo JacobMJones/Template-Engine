c4f1aa74d074818fab13d48ed10599f7b887d0f5
JacobMJones <theretherehuman@gmail.com>
2018-05-15 19:00:23 +0000

    complete

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ app.get('/', function(req, res) {
         { name: 'Martini', drunkness: 5 },
         { name: 'Scotch', drunkness: 10 }
     ];
-    var tagline = ""Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+    var tagline2 = "yo im another tagline";
 	res.render('pages/index', {
         drinks: drinks,
-        tagline: tagline
+        tagline: tagline,
+        tagline2: tagline2
     });
 });
 
